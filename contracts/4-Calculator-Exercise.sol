03e780cf22ac10b69925462d022d354199bcf157
Bhuwan138 <pandeybhuwan138@gmail.com>
2024-10-31 22:51:54 +0545

    calculator added

--- contracts/4-Calculator-Exercise.sol
+++ contracts/4-Calculator-Exercise.sol
@@ -8,6 +8,22 @@ pragma solidity ^0.8.0;
 // 4️⃣ Create a function to get result
 
 contract Calculator {
-    uint256 public result;
+    uint256 result = 0;
+
+    function add(uint256 num) public {
+        result += num;
+    }
+
+    function subtract(uint256 num) public {
+        result -= num;
+    }
+
+    function multiply(uint256 num) public {
+        result *= num;
+    }
+
+    function get() public view returns (uint256){
+        return result;
+    }
 
 }
